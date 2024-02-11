4bd9b3b87847cfe4745895dbb60ce5cac28affa7
Marc Bäckman <marcbackman.lapinamk@gmail.com>
2024-02-11 19:00:19 +0200

    added route for speech-to-text-to-summary

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import json
 import os
-
-import sparknlp
 import sparknlp_jsl
 from sparknlp_jsl.annotator import *
 import pandas as pd
 import warnings
 from sparknlp.pretrained import ResourceDownloader
 import subprocess
+from laymen_summary import summarizer_clinical_laymen_onnx_pipeline
+from dotenv import load_dotenv
+from flask import Flask, request
+from flask_cors import CORS
+import whisper
 
-from data import input1, input2, input3, input4
-from laymen_summary import summarizer_clinical_laymen_onnx_pipeline, summarizer_clinical_laymen_onnx, \
-    summarizer_clinical_laymen_pipeline, summarizer_clinical_laymen
+app = Flask(__name__)
+CORS(app)
+load_dotenv()
 
+# temporary folder to store uploaded file
+UPLOAD_FOLDER = 'C:/temp_whisper_uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def get_java_version():
     try:
@@ -40,6 +46,53 @@ def start_spark_session():
 
     return sparknlp_jsl.start(license_keys['SECRET'], params=params)
 
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/stt', methods=['POST'])
+def speech_to_text_api():
+    try:
+        if 'speech' not in request.files:
+            return 'No file part'
+
+        audio_file = request.files['speech']
+        if audio_file.filename == '':
+            return 'No selected file'
+
+        # Save the uploaded file with a unique filename
+        unique_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'uploaded_audio.wav')
+        audio_file.save(unique_filename)
+
+        model = whisper.load_model("base")
+
+        # load audio and pad/trim it to fit 30 seconds
+        audio = whisper.load_audio(unique_filename)
+        audio = whisper.pad_or_trim(audio)
+
+        # make log-Mel spectrogram and move to the same device as the model
+        mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+        # detect the spoken language
+        _, probs = model.detect_language(mel)
+        print(f"filename: {audio_file.filename}")
+        print(f"Detected language: {max(probs, key=probs.get)}")
+
+        # decode the audio
+        options = whisper.DecodingOptions(fp16=False)
+        result = whisper.decode(model, mel, options)
+
+        # summarize the text
+        summarizer_clinical_laymen_onnx_pipeline(result.text)
+        return result.text
+
+
+    except Exception as e:
+        return f'Error: {e}'
+
+
 if __name__ == "__main__":
     # Start the Spark session
     spark_session = start_spark_session()
@@ -54,12 +107,4 @@ if __name__ == "__main__":
     print("Spark NLP Version :", sparknlp.version())
     print("Spark NLP_JSL Version :", sparknlp_jsl.version())
 
-    # Show summaries
-    input('\nPRESS ENTER TO CONTINUE\n')
-    summarizer_clinical_laymen_onnx_pipeline(input1)
-    input('\nPRESS ENTER TO CONTINUE\n')
-    summarizer_clinical_laymen_onnx(spark_session, input2)
-    input('\nPRESS ENTER TO CONTINUE\n')
-    summarizer_clinical_laymen_pipeline(input3)
-    input('\nPRESS ENTER TO CONTINUE\n')
-    summarizer_clinical_laymen(spark_session, input4)
+    app.run(debug=True)
